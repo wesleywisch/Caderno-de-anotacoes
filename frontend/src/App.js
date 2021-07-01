76af8fb560c866d5d3dc27177516c907a3e51b22
wesley_wisch <wesley_wisch@hotmail.com>
2021-07-01 07:44:32 -0400

    configurando o frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div id="app">
+      <aside>
+        
+      </aside>
+
+      <main>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
