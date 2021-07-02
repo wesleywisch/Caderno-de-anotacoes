4662cd64c0a91359e8c246d3b2bc77e3be3a8808
wesley_wisch <wesley_wisch@hotmail.com>
2021-07-02 07:53:37 -0400

    Pequena modificação, pois estava quebrando o layout

--- BackEnd/src/controllers/ContentController.js
+++ BackEnd/src/controllers/ContentController.js
@@ -5,13 +5,12 @@ module.exports = {
 
     async update(req, res){
         const { id } = req.params;
-        const { title, notes } = req.body;
+        const { notes } = req.body;
 
         const annotation = await Annotations.findOne({ _id: id });
 
         if(notes || title){
             annotation.notes = notes;
-            annotation.title = title;
 
             await annotation.save();
         }
