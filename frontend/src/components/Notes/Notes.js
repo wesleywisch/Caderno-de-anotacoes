e6ed3a874bbb093180cb04ad2abc5b0bc282413a
wesley_wisch <wesley_wisch@hotmail.com>
2021-07-02 07:52:38 -0400

    Função para poder alterar o conteudo do cartão

--- frontend/src/components/Notes/Notes.js
+++ frontend/src/components/Notes/Notes.js
@@ -1,9 +1,36 @@
+import { useState } from 'react';
 import { AiTwotoneDelete, AiOutlineExclamationCircle } from 'react-icons/ai';
 
 import './style.css';
 import './style-priority.css';
 
+import api from '../services/api';
+
 export default function Note({ data }){
+    const [chengedNote, setChengedNote] = useState('');
+
+    async function handleSave(e, notes){
+        e.style.cursor = 'default';
+        e.style.boxShadow = 'none';
+
+        if (chengedNote && chengedNote !== notes){
+            await api.post(`/contents/${data._id}`, {
+                notes : chengedNote,
+            })
+        }
+    }
+
+    function handleEdit(e, priority){
+        e.style.cursor = 'text';
+        e.style.borderRadius = '5px';
+
+        if(priority){
+            e.style.boxShadow = '0 0 5px white';
+        }else{
+            e.style.boxShadow = '0 0 5px gray';
+        }
+    }
+
     return(
         <>
             <li className={data.priority ? "notepad-infos-priority" : "notepad-infos"}>
@@ -15,7 +42,12 @@ export default function Note({ data }){
                     </div>
                 </div>
 
-                <textarea defaultValue={data.notes} ></textarea>
+                <textarea 
+                defaultValue={data.notes} 
+                onClick={e => handleEdit(e.target, data.priority)}
+                onChange={e => setChengedNote(e.target.value)}
+                onBlur={e => handleSave(e.target, data.notes)}
+                />
 
                 <span>
                     <AiOutlineExclamationCircle size="24" />
